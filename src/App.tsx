993b0f1f164711a91d276704604d3767bd661287
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2023-10-17 15:42:09 +0330

    Add theme provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Container, GlobalStyles } from "./Styled-Components/StyledContainer";
 import Navbar from "./Components/Navbar";
 import Introduction from "./Components/Introduction";
 import AboutMe from "./Components/AboutMe";
+import { rootTypes, store } from "./Context/Store";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./Theme/theme";
 
 function App() {
+  const { changeTheme } = useContext(store) as rootTypes;
+
   return (
-    <React.Fragment>
-      <GlobalStyles />
-      <Container>
-        <Navbar />
-        <Introduction />
-        <AboutMe />
-      </Container>
-    </React.Fragment>
+    <ThemeProvider theme={changeTheme ? lightTheme : darkTheme}>
+      <React.Fragment>
+        <GlobalStyles />
+        <Container>
+          <Navbar />
+          <Introduction />
+          <AboutMe />
+        </Container>
+      </React.Fragment>
+    </ThemeProvider>
   );
 }
 
